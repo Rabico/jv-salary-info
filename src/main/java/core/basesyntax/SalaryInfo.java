a7a4f15883aff8161494bb9bd89046ae63565bce
Przemysław Ciżmiński <cizminski@interia.pl>
2025-11-05 05:41:22 +0100

    Changed variables

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,20 +4,20 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_INDEX_0 = 0;
+    private static final int DATE_INDEX_1 = 1;
+    private static final int DATE_INDEX_2 = 2;
+    private static final int DATE_INDEX_3 = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
         LocalDate dateFromLocal;
         LocalDate dateToLocal;
         LocalDate dateActualLocal;
         String[] split = new String[4];
         int salary;
-        int zero = 0;
-        int one = 1;
-        int two = 2;
-        int three = 3;
-
         dateFromLocal = LocalDate.parse(dateFrom, formatter);
         dateToLocal = LocalDate.parse(dateTo, formatter);
         builder = new StringBuilder();
@@ -32,16 +32,17 @@ public class SalaryInfo {
 
             builder.append(System.lineSeparator())
                     .append(name);
-            salary = zero;
+            salary = DATE_INDEX_0;
             for (String dates : data) {
                 split = dates.split(" ");
-                dateActualLocal = LocalDate.parse(split[zero], formatter);
+                dateActualLocal = LocalDate.parse(split[DATE_INDEX_0], formatter);
                 if (((dateFromLocal.isBefore(dateActualLocal)
                         && dateActualLocal.isBefore(dateToLocal))
                         || dateFromLocal.isEqual(dateActualLocal)
                         || dateActualLocal.isEqual(dateToLocal))
-                        && name.equals(split[one])) {
-                    salary += Integer.parseInt(split[two]) * Integer.parseInt(split[three]);
+                        && name.equals(split[DATE_INDEX_1])) {
+                    salary += Integer.parseInt(split[DATE_INDEX_2])
+                            * Integer.parseInt(split[DATE_INDEX_3]);
                 }
 
             }
